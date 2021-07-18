83bbff9c5a4d33b915fd7407007d98c497fab883
Ali Razmdideh <ali.97.razmdideh@gmail.com>
2021-07-18 20:30:22 +0430

    Add custom action to retrieve from dataframe

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -6,22 +6,42 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
+import pandas as pd
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
+
+class DataFrameRetrieve(Action):
+
+    def name(self) -> Text:
+        return "data_frame_retrieve"
+
+    def run(self, 
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        drug = tracker.get_slot('drug')
+        intent = tracker.latest_message['intent']
+        df = pd.read_csv('drugs_dataset.csv')
+        result = df[df['medicine'] == drug][intent]
+
+        dispatcher.utter_message(result[0])
+        return []
+
+
+# from rasa_sdk.events import AllSlotsReset
+
 # class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+#      def name(self) -> Text:
+#             return "action_hello_world"
+
+#      def run(self, dispatcher: CollectingDispatcher,
+#              tracker: Tracker,
+#              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#          dispatcher.utter_message("Hello World!")
+
+#          return [AllSlotsReset()]
