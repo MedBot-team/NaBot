095254a25bc672c5a7291c4fb31fd05560a459ef
mehdi <mohammadmheidari@outlook.com>
2021-08-16 00:54:58 +0430

    add telegram interface

--- production/rasa-server/rasa/run_rasa.sh
+++ production/rasa-server/rasa/run_rasa.sh
@@ -10,8 +10,9 @@ python -c "import sqlalchemy;\
 sqlalchemy.create_engine('mysql://${SQL_USER}:${MYSQL_ROOT_PASSWORD}@${DB_HOST}')\
 .execute('CREATE DATABASE IF NOT EXISTS ${MYSQL_EVENTS_DATABASE}')"
 
-# Substitute environment variables into endpoints file
+# Substitute environment variables into endpoints and credentials files
 envsubst < endpoints.yml | tee endpoints.yml > /dev/null
+envsubst < credentials.yml | tee credentials.yml > /dev/null
 
 # Run rasa command
 rasa run --log-file logs/rasa-server.log --enable-api --auth-token ${TOKEN}
