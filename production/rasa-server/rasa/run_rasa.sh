46b460bd57a65b72737fa8dd705c785a15977465
Ali Razmdideh <24594649+Ali-Razmdideh@users.noreply.github.com>
2021-08-16 18:10:21 +0430

    Fix empty output bug in endpoints and credentials
    This bug happens due to the asynchronous property of the pipe

--- production/rasa-server/rasa/run_rasa.sh
+++ production/rasa-server/rasa/run_rasa.sh
@@ -10,9 +10,17 @@ python -c "import sqlalchemy;\
 sqlalchemy.create_engine('mysql://${SQL_USER}:${MYSQL_ROOT_PASSWORD}@${DB_HOST}')\
 .execute('CREATE DATABASE IF NOT EXISTS ${MYSQL_EVENTS_DATABASE}')"
 
-# Substitute environment variables into endpoints and credentials files
-envsubst < endpoints.yml | tee endpoints.yml > /dev/null
-envsubst < credentials.yml | tee credentials.yml > /dev/null
+# List of files which will be modified 
+declare -a files=("endpoints.yml" "credentials.yml")
+
+# Substitute environment variables into the files
+for file in "${files[@]}"; do
+  # Create a temporary file. This is needed because of asynchronous property of the pipe
+  tmpfile=$(mktemp)
+  cp --attributes-only --preserve $file $tmpfile
+  envsubst < $file > $tmpfile 
+  mv $tmpfile $file
+done
 
 # Run rasa command
 rasa run --log-file logs/rasa-server.log --enable-api --auth-token ${TOKEN}
