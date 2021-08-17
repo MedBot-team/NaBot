90f9b19cd6dbf054edc52a8cc3916e5fec5afb8d
Ali Razmdideh <ali.97.razmdideh@gmail.com>
2021-08-18 01:50:00 +0430

    Update monitoring script with more statistics

--- production/monitoring-server/monitoring_ui.py
+++ production/monitoring-server/monitoring_ui.py
@@ -46,6 +46,8 @@ def get_confidences(datas):
     intent_confidences = []
     entity_confidences = []
     timestamps = []
+    input_channels = []
+    entity_extractors = []
 
     for i in range(data_count):
         json_acceptable_string = datas[i].replace("'", "\"")
@@ -58,14 +60,22 @@ def get_confidences(datas):
                 entity_confidences.append(entity['confidence_entity'])
 
         timestamps.append(dictionary['timestamp'])
+        input_channels.append(dictionary['input_channel'])
+        entity_extractors += [entity['extractor'] for entity in dictionary['parse_data']['entities']]
 
-    return intent_confidences, entity_confidences, timestamps
+    return intent_confidences, entity_confidences, timestamps, input_channels, entity_extractors
 
 
 def convert_date(timestamps):
     dates = [datetime.fromtimestamp(timestamp).strftime(
         '%Y-%m-%d') for timestamp in timestamps]
     return dates
+
+
+def get_feedbacks(intents):
+    feedbacks = [intent for intent in intents if intent in [
+        'good_response', 'bad_response']]
+    return feedbacks
 
 
 matplotlib.use("agg")
@@ -78,24 +88,72 @@ st.text('Analyzing Chatbot Records')
 events = get_events()
 intents = get_intents(events)
 datas = get_datas(events)
-intent_confidences, entity_confidences, timestamps = get_confidences(datas)
+intent_confidences, entity_confidences, timestamps, input_channels, entity_extractors = get_confidences(datas)
 dates = convert_date(timestamps)
+feedbacks = get_feedbacks(intents)
+
 
 st.write('')
 row1_space1, row1_1, row1_space2, row1_2, row1_space3 = st.columns(
     (.1, 1, .1, 1, .1))
 
 with row1_1, _lock:
-    st.subheader('Intent Confidence Distribution')
+    st.subheader('Input channels statistics')
     fig = Figure()
     ax = fig.subplots()
-    sns.histplot(intent_confidences, color="dodgerblue",
+    sns.histplot(input_channels, color="turquoise", kde=True,
+                 stat="density", linewidth=0, ax=ax)
+
+    xticks = ax.get_xticks()
+    ax.set_xticks(xticks[::len(xticks) // 2])
+    st.pyplot(fig)
+    st.markdown("Chatbot requests over time")
+
+
+with row1_2, _lock:
+    st.subheader('Feedback state')
+    fig = Figure()
+    ax = fig.subplots()
+    sns.histplot(feedbacks, color="dodgerblue",
                  kde=True, stat="density", linewidth=0, ax=ax)
     st.pyplot(fig)
     st.markdown("Distribution of intents confidences")
 
 
-with row1_2, _lock:
+st.write('')
+row2_space1, row2_1, row2_space2, row2_2, row2_space3 = st.columns(
+    (.1, 1, .1, 1, .1))
+
+with row2_1, _lock:
+    st.subheader('Chatbot requests over time')
+    fig = Figure()
+    ax = fig.subplots()
+    sns.histplot(dates, color="orange", kde=True,
+                 stat="density", linewidth=0, ax=ax)
+
+    xticks = ax.get_xticks()
+    ax.set_xticks(xticks[::len(xticks) // 2])
+    ax.tick_params(axis='x', rotation=70)
+    st.pyplot(fig)
+    st.markdown("Chatbot requests over time")
+
+with row2_2, _lock:
+    st.subheader('Intents Distribution')
+    fig = Figure(figsize=(8, 6))
+    ax = fig.subplots()
+    ax.hist(intents, density=True, bins=30, color='deepskyblue')
+    ax.set_xlabel('Intents')
+    ax.set_ylabel('Density')
+    plt.setp(ax.xaxis.get_majorticklabels(), rotation=70)
+    st.pyplot(fig)
+    st.markdown("Distribution of intents")
+
+
+st.write('')
+row3_space1, row3_1, row3_space2, row3_2, row3_space3 = st.columns(
+    (.1, 1, .1, 1, .1))
+
+with row3_1, _lock:
     st.subheader('Entity Confidence Distribution')
     fig = Figure()
     ax = fig.subplots()
@@ -105,34 +163,25 @@ with row1_2, _lock:
     st.markdown("Distribution of entities confidences")
 
 
-st.write('')
-row2_space1, row2_1, row2_space2, row2_2, row2_space3 = st.columns(
-    (.1, 1, .1, 1, .1))
-
-with row2_1, _lock:
-    st.subheader('Intents Distribution')
-    fig = Figure(figsize=(8, 6))
-    ax = fig.subplots()
-    ax.hist(intents, density=True, bins=30)
-    ax.set_xlabel('Intents')
-    ax.set_ylabel('Density')
-    plt.setp(ax.xaxis.get_majorticklabels(), rotation=70)
-    st.pyplot(fig)
-    st.markdown("Distribution of intents")
-
-
-with row2_2, _lock:
-    st.subheader('Chatbot requests over time')
-
+with row3_2, _lock:
+    st.subheader('Intent Confidence Distribution')
     fig = Figure()
     ax = fig.subplots()
-    sns.histplot(dates, color="orange", kde=True,
-                 stat="density", linewidth=0, ax=ax);
-
-    xticks = ax.get_xticks()
-    ax.set_xticks(xticks[::len(xticks) // 2]) 
-    ax.tick_params(axis='x', rotation=70) 
-
-
+    sns.histplot(intent_confidences, color="dodgerblue",
+                 kde=True, stat="density", linewidth=0, ax=ax)
     st.pyplot(fig)
-    st.markdown("Chatbot requests over time")
+    st.markdown("Distribution of intents confidences")
+
+
+st.write('')
+row4_space1, row4_1, row4_space2, row4_2, row4_space3 = st.columns(
+    (.1, 1, .1, 1, .1))
+
+with row4_1, _lock:
+    st.subheader('Entity extractors statistics')
+    fig = Figure()
+    ax = fig.subplots()
+    sns.histplot(entity_extractors, color="darkviolet",
+                 kde=True, stat="density", linewidth=0, ax=ax)
+    st.pyplot(fig)
+    st.markdown("Distribution of entities confidences")
