0a3753b39f435823cfe9898ec20307d2a7da2f6b
mehdi <mohammadmheidari@outlook.com>
2021-09-05 23:43:16 +0430

    adjust retrieveer.py with database connector (not fully functional yet)

--- production/action-server/actions/retriever.py
+++ production/action-server/actions/retriever.py
@@ -4,7 +4,8 @@ from decouple import config
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from .intent_map import intent_map
+from .intent_map import get_columns
+from .database import DatabaseConnector
 
 class Retrieve(Action):    
     def __init__(self) -> None:
@@ -14,20 +15,19 @@ class Retrieve(Action):
         user = config('SQL_USER')
         host = config('DATASETS_DB_HOST')
         database = config('MYSQL_DATASETS_DATABASE')
-
-        self.table = config('LAB_TABLE')
-        self.db = mysql.connector.connect(
-            host=host,
-            user=user,
-            password=password,
-            database=database)
-        self.like_buttons = [
-                {"payload": "/good_response", "title": "👍🏻"},
-                {"payload": "/bad_response", "title": "👎🏻"},
-                ]
-        self.addition_button = [{"payload": "/addition_request", 
-                                 "title": "request addition to database"},]
-        self.button_type='inline'
+        
+        self.db = DatabaseConnector(host=host,
+                                    user=user,
+                                    password=password,
+                                    database=database,
+                                    )
+        # self.like_buttons = [
+        #         {"payload": "/good_response", "title": "👍🏻"},
+        #         {"payload": "/bad_response", "title": "👎🏻"},
+        #         ]
+        # self.addition_button = [{"payload": "/addition_request", 
+        #                          "title": "request addition to database"},]
+        # self.button_type='inline'
     
     def name(self) -> Text:
         return "retrieve"
@@ -36,10 +36,39 @@ class Retrieve(Action):
             dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        # if tracker.get_intent_of_latest_message() == 'what_is':
-        intent = tracker.latest_message['intent']['name']
-        entity = tracker.get_slot('entity_name')
-        dispatcher.utter_message(
-            text = f'intent:{intent}, entity:{entity}',
-            )
         
+        entity = self.get_entity(tracker=tracker)
+        intent = tracker.get_slot('intent_name')
+        
+        if entity is not None:
+            drugs = self.db.search_drug(entity)
+            labs = self.db.search_lab(entity)
+            if drugs and labs:
+                dispatcher.utter_message(
+                    text = 'entity is in both drug and lab',
+                    )
+            elif drugs:
+                column = get_columns(intent, 'drug')[0]
+                answer = self.db.retrieve_drug(column=column, drug_name=entity)
+                print(entity)
+                dispatcher.utter_message(text=answer[0])
+            elif labs:
+                column = get_columns(intent, 'lab')[0]
+                answer = self.db.retrieve_lab(column=column, drug_name=entity)
+                dispatcher.utter_message(text=answer[0])
+            else:
+                dispatcher.utter_message(response='utter_not_found')
+               
+        else:
+            dispatcher.utter_message(response='utter_not_found')
+        # dispatcher.utter_message(
+        #     text = f'intent:{intent}, entity:{entity[-1]}',
+        #     )
+    
+    @staticmethod
+    def get_entity(tracker: Tracker):
+        entity = tracker.get_slot('entity_name')
+        if isinstance(entity, list):
+            return entity[-1]
+        else:
+            return entity
