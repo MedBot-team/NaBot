5bf380856d45c7a128f6c22b21b5a4bd0c23a406
mehdi <mohammadmheidari@outlook.com>
2021-09-22 17:06:21 +0330

    add request function

--- production/ui-server/flask_server/app.py
+++ production/ui-server/flask_server/app.py
@@ -2,6 +2,10 @@ import requests
 import json
 from flask import Flask, render_template, request
 
+# url = config('RASA_SERVER_URL')
+headers = {
+                'Content-Type': 'application/json'
+            }
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -9,6 +13,18 @@ app.static_folder = 'static'
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    user_text = request.args.get('msg')
+    payload = json.dumps({
+                "message": f"{user_text}",
+                "sender": f"me"
+            })
+    response = requests.request(
+                "POST", url, headers=headers, data=payload)
+    
+    return response.json()[0]['text'].replace('\n', '<br>')
 
 if __name__ == "__main__":
     app.run()
