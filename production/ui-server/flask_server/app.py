4178ad4805180424f63e83eee514c9e2aecadc12
mehdi <mohammadmheidari@outlook.com>
2021-09-26 16:02:51 +0330

    add chitchat to ui

--- production/ui-server/flask_server/app.py
+++ production/ui-server/flask_server/app.py
@@ -3,7 +3,9 @@ import json
 from decouple import config
 from flask import Flask, render_template, request
 
-url = config('RASA_SERVER_URL')
+rasa_url = config('RASA_SERVER_URL')
+chitchat_url = config('CHITCHAT_SERVER_URL')
+chitchat_api_key = config('CHITCHAT_API_KEY')
 headers = {
                 'Content-Type': 'application/json'
             }
@@ -17,15 +19,24 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
+    t = request.args.get('chat_type')
     user_text = request.args.get('msg')
-    payload = json.dumps({
-                "message": f"{user_text}",
-                "sender": f"me"
-            })
-    response = requests.request(
-                "POST", url, headers=headers, data=payload)
-    
-    return response.json()[0]['text'].replace('\n', '<br>')
+    if t=='false':
+        payload = json.dumps({
+                    "message": f"{user_text}",
+                    "sender": f"me"
+                })
+        response = requests.request(
+                    "POST", rasa_url, headers=headers, data=payload)
+        return response.json()[0]['text'].replace('\n', '<br>')
+    else:
+        payload = json.dumps({
+                    "utterance": f"{user_text}",
+                    "api_key":chitchat_api_key
+                })
+        response = requests.request(
+                    "POST", chitchat_url, headers=headers, data=payload)
+        return response.json()['reply'].replace('\n', '<br>')
 
 if __name__ == "__main__":
     from waitress import serve
