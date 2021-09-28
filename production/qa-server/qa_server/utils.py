19d422e7031277c9ab44356ba0339e0423b07133
mehdi <mohammadmheidari@outlook.com>
2021-09-28 16:18:07 +0330

    script now reads config from file

--- production/qa-server/qa_server/utils.py
+++ production/qa-server/qa_server/utils.py
@@ -1,4 +1,12 @@
 from transformers import pipeline
+from ruamel import yaml
+
+# Loads config from config.yml file
+def load_conf():
+    CONFIG_FILE_ADR = './config.yml'
+    with open(CONFIG_FILE_ADR, 'r') as f:
+        conf = yaml.load(f, Loader=yaml.RoundTripLoader)
+    return conf
     
 class QuestionAnswering():
     def __init__(self):
