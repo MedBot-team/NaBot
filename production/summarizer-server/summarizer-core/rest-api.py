38b1f5455bc762936a1d3799ad5daece3ab44d9f
mehdi <mohammadmheidari@outlook.com>
2021-10-04 14:44:28 +0330

    change summarizer server's port

--- production/summarizer-server/summarizer-core/rest-api.py
+++ production/summarizer-server/summarizer-core/rest-api.py
@@ -53,4 +53,4 @@ def Summarize():
 # Run flask APP in production mode
 if __name__ == '__main__':
     from waitress import serve
-    serve(app, host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=9090)
